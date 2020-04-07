6e121b7ffb5aa18b3ca9e27880d71f538a256d16
prashant kumar singh <singh.prashantking@gmail.com>
2020-04-08 00:37:18 +0530

    add state to get data from API and Remove default margin padding in css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Cards, Chart, CountryPicker } from "./components";
+import { fetchData } from "./api";
 
-function App() {
-  return (
-    <div className="App">
-      <Cards />
-      <CountryPicker />
-      <Chart />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+    console.log(fetchedData);
+  }
+
+  render() {
+    const { data } = this.state;
+    return (
+      <div className="App">
+        <Cards data={data} />
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
+  }
 }
 
 export default App;
