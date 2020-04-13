ab1b603a2f40367094eff6512b744cd16e7413bf
prashant kumar singh <singh.prashantking@gmail.com>
2020-04-13 15:04:12 +0530

    Updates

---
+++ precache-manifest.bcbb453d49a1e07d7bf83bf5dc7ee81b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9380ffa73f005fb04829c98999d8d83e",
+    "url": "/COVID-19/index.html"
+  },
+  {
+    "revision": "ae5ad1c325049d29298c",
+    "url": "/COVID-19/static/css/main.867e659f.chunk.css"
+  },
+  {
+    "revision": "09ec7661a33b6ebaabfb",
+    "url": "/COVID-19/static/js/2.cc2abaa3.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19/static/js/2.cc2abaa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae5ad1c325049d29298c",
+    "url": "/COVID-19/static/js/main.f1c726f8.chunk.js"
+  },
+  {
+    "revision": "23188568c9bcdc9cdeae",
+    "url": "/COVID-19/static/js/runtime-main.055a0d2d.js"
+  },
+  {
+    "revision": "e7acb4cf5b7a6c373fc75f92bf40be1d",
+    "url": "/COVID-19/static/media/death.e7acb4cf.svg"
+  },
+  {
+    "revision": "4e113c7e2f71c27ff4f71c6d1444df6c",
+    "url": "/COVID-19/static/media/infected.4e113c7e.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/COVID-19/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c00aaeb3d88f36da8b9e9437d97f7750",
+    "url": "/COVID-19/static/media/recovered.c00aaeb3.svg"
+  }
+]);
